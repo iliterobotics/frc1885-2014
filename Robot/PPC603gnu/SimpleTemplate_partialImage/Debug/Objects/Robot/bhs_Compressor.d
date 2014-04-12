5d55b014ebbfb867214dc9d9b8d6d764a3e7caa0
Silu Tang <silu.na.tang@gmail.com>
2014-04-12 12:58:47 -0400

    Removed All of the object files

--- Robot/PPC603gnu/SimpleTemplate_partialImage/Debug/Objects/Robot/bhs_Compressor.d
+++
@@ -1,2 +0,0 @@
-SimpleTemplate_partialImage/Debug/Objects/Robot/bhs_Compressor.o:  \
- C:/Users/Silu/GitHub/frc1885-2014/Robot/bhs_Compressor.cpp
--- Robot/PPC603gnu/SimpleTemplate_partialImage/Debug/Objects/Robot/bhs_GDDrivetrain.d
+++
@@ -1,8 +0,0 @@
-SimpleTemplate_partialImage/Debug/Objects/Robot/bhs_GDDrivetrain.o:  \
- C:/Users/Silu/GitHub/frc1885-2014/Robot/bhs_GDDrivetrain.cpp \
-  C:/Users/Silu/GitHub/frc1885-2014/Robot/bhs_GDDrivetrain.h \
-  C:/Users/Silu/GitHub/frc1885-2014/Robot/bhs_Constants.h
-
-C:/Users/Silu/GitHub/frc1885-2014/Robot/bhs_GDDrivetrain.h:
-
-C:/Users/Silu/GitHub/frc1885-2014/Robot/bhs_Constants.h:
